e2583c592d1db89597aa5739f421b24994cb4c36
Chisomo <107890763+chizzywizzy833@users.noreply.github.com>
2024-06-05 13:02:16 +1200

    Base Skeleton for Score Tracker

---
+++ 00_ST_Base_v0.py
@@ -0,0 +1,14 @@
+"""
+base component for Score Tracker
+V0 - skeleton setup
+author- Chisomo Kamphambe
+ST CLK 2024
+"""
+
+# Import libraries go here **************************
+
+
+# functions go here **************************
+
+
+# main routine goes here ***************
